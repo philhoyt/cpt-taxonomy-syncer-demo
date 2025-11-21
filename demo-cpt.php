e8853c19a5b5ab5e8d5e037e9a4164a147c976ff
Phil Hoyt <philhoyt@users.noreply.github.com>
2025-11-20 22:36:50 -0500

    Update demo-cpt.php

--- demo-cpt.php
+++ demo-cpt.php
@@ -119,3 +119,62 @@ function demo_cpt_admin_notice() {
 }
 add_action( 'admin_notices', 'demo_cpt_admin_notice' );
 
+/**
+ * Show admin notice to create test content after sync is configured
+ */
+function demo_cpt_test_content_notice() {
+	// Only show on admin pages
+	if ( ! is_admin() ) {
+		return;
+	}
+
+	// Check if CPT-Taxonomy Syncer is active
+	if ( ! function_exists( 'cpt_taxonomy_syncer_init' ) ) {
+		return;
+	}
+
+	// Check if sync pair is configured
+	$pairs = get_option( 'cpt_tax_syncer_pairs', array() );
+	$is_configured = false;
+	foreach ( $pairs as $pair ) {
+		if ( isset( $pair['cpt_slug'] ) && $pair['cpt_slug'] === 'habitats' 
+			&& isset( $pair['taxonomy_slug'] ) && $pair['taxonomy_slug'] === 'habitat_types' ) {
+			$is_configured = true;
+			break;
+		}
+	}
+
+	// Only show if sync is configured
+	if ( ! $is_configured ) {
+		return;
+	}
+
+	// Check if there are already Habitats posts
+	$habitats_count = wp_count_posts( 'habitats' );
+	$has_posts = isset( $habitats_count->publish ) && $habitats_count->publish > 0;
+
+	// Don't show if they already have posts
+	if ( $has_posts ) {
+		return;
+	}
+
+	// Show notice
+	$new_post_url = admin_url( 'post-new.php?post_type=habitats' );
+	?>
+	<div class="notice notice-success is-dismissible">
+		<p>
+			<strong><?php esc_html_e( 'Sync Pair Configured!', 'cpt-taxonomy-syncer-demo' ); ?></strong>
+		</p>
+		<p>
+			<?php esc_html_e( 'Great! Now try creating a new Habitats post to see the syncing in action. When you create a Habitats post, a corresponding "Habitat Types" term will be automatically created (and vice versa).', 'cpt-taxonomy-syncer-demo' ); ?>
+		</p>
+		<p>
+			<a href="<?php echo esc_url( $new_post_url ); ?>" class="button button-primary">
+				<?php esc_html_e( 'Create a Habitats Post', 'cpt-taxonomy-syncer-demo' ); ?>
+			</a>
+		</p>
+	</div>
+	<?php
+}
+add_action( 'admin_notices', 'demo_cpt_test_content_notice' );
+
